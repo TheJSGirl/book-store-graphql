31884296672f37f1fe17986be8232a6641566bb4
Priyanka Negi <prinegi100@gmail.com>
2018-04-15 09:01:38 +0530

    -export schema

--- schema/index.js
+++ schema/index.js
@@ -1,15 +1,31 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString}= graphql;
+const { GraphQLObjectType, GraphQLString, GraphQlSchema } = graphql;
 
-const bookType = new GraphQLObjectType({
+const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: {
       type: GraphQLString,
       name: GraphQLString,
-      genre: GraphQLString,
-      
+      genre: GraphQLString
     }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //to get data from db or other source
+      }
+    }
+  }
+});
+
+module.exports = new GraphQlSchema({
+  query: RootQuery
 });
