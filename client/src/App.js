f12830dbd0b38ca12b6c34c8f45fdf2dffea750e
Priyanka Negi <prinegi100@gmail.com>
2018-04-18 00:17:30 +0530

    -removed yarn.lock & created BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import BookList from './component/BookList';
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>List</h1>
+        <BookList />
       </div>
     );
   }
